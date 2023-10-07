cd901a1dbf1bec711e31a0964c456cc3768627c2
Tejas <hntejas@gmail.com>
2023-10-07 18:12:01 +0530

    fixes

--- src/components/BookInvite.jsx
+++ src/components/BookInvite.jsx
@@ -3,15 +3,26 @@ import "./book-invite.css";
 
 const PAGES = [
   {
-    top: <div className="content"><img className="image-1" src="images/page-1-top.png" /></div>,
+    top: (
+      <div className="content">
+        <img className="image-1" src="images/page-1-top.png" />
+      </div>
+    ),
     bottom: (
       <div className="content">
-        <h2>We cordially invite you to this special occasion</h2>
+        <h2 className="page-1-text">
+          We cordially invite you to grace the occasion of our marriage and seek
+          your best wishes as we embark on this new chapter in our life.
+        </h2>
       </div>
-    )
+    ),
   },
   {
-    top: <div className="content"><img className="image-1" src="images/page-2-top.png" /></div>,
+    top: (
+      <div className="content">
+        <img className="image-1" src="images/page-2-top.png" />
+      </div>
+    ),
     bottom: (
       <div className="content">
         <h2>Reception :</h2>
@@ -20,10 +31,14 @@ const PAGES = [
         <h2>Muhurtha :</h2>
         <h2>19th October, 9-10AM</h2>
       </div>
-    )
+    ),
   },
   {
-    top: <div className="content"><img className="image-1" src="images/page-3-top.png" /></div>,
+    top: (
+      <div className="content">
+        <img className="image-1" src="images/page-3-top.png" />
+      </div>
+    ),
     bottom: (
       <div className="content">
         <h2>Shivabalayougi kalyana mantapa (upper hall)</h2>
@@ -31,11 +46,10 @@ const PAGES = [
         <a href="https://maps.app.goo.gl/qhL2AyNoRoVK9fEZA">
           <img className="map-logo" src="images/g-map.png" />
         </a>
-
       </div>
-    )
-  }
-]
+    ),
+  },
+];
 
 const BookInvite = () => {
   const [showUpAnimation, setShowUpAnimation] = useState(false);
@@ -66,7 +80,7 @@ const BookInvite = () => {
     setTimeout(() => {
       setShowUpAnimation(false);
     }, 1000);
-  }
+  };
 
   const handleBottomPageClick = (e, isAuto) => {
     if (currentPage === PAGES.length - 1) return;
@@ -90,24 +104,23 @@ const BookInvite = () => {
     setTimeout(() => {
       setShowDownAnimation(false);
     }, 1000);
-  }
+  };
 
   useEffect(() => {
     if (autoIsOn) {
       timerRef.current = setTimeout(() => {
-        autoIsOn && handleBottomPageClick({}, true)
+        autoIsOn && handleBottomPageClick({}, true);
       }, 4000);
     }
-
 
     if (currentPage === PAGES.length - 1 && autoIsOn) {
       setAutoIsOn(false);
       clearTimeout(timerRef.current);
-    };
+    }
 
     return () => {
       clearTimeout(timerRef.current);
-    }
+    };
   }, [currentPage, autoIsOn]);
 
   useEffect(() => {
@@ -125,13 +138,15 @@ const BookInvite = () => {
       {showUpAnimation && <div className="psuedoBookDown" />}
       {showDownAnimation && <div className="psuedoBook" />}
       <div className="book">
-        <div className="page page-up" onClick={handleTopPageClick}>{PAGES[currentPage].top}</div>
+        <div className="page page-up" onClick={handleTopPageClick}>
+          {PAGES[currentPage].top}
+        </div>
         <div className="page page-down" onClick={handleBottomPageClick}>
           {PAGES[currentPage].bottom}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default BookInvite;
