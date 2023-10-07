cbbfa7f1963592cee30366637ce31a07edb09f18
Tejas <hntejas@gmail.com>
2023-10-07 17:13:29 +0530

    fixes

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,16 +1,18 @@
 import "./main-page.css";
 
-const MainPage = ({handleClick}) => {
-  return <div className="book-cover" onClick={handleClick}>
-    <img src="images/top-book-cover.jpg" />
-    <h1 className="names">
-      Tejas & Vanishree
-    </h1>
-    <h2 className="date">
-      19 / 10 / 2023
-    </h2>
-    <img src="images/top-book-cover.jpg" />
-  </div>
-}
+const MainPage = ({ handleClick }) => {
+  return (
+    <div className="book-cover" onClick={handleClick}>
+      <img src="images/top-book-cover.jpg" />
+      <h1 className="names">Tejas & Vanishree</h1>
+      <h2 className="date">19 / 10 / 2023</h2>
+      <img src="images/top-book-cover.jpg" />
+
+      <img className="hide" src="images/wood-bg.png" />
+      <img className="hide" src="images/red-3.jpg" />
+      <img className="hide" src="images/pattern-top.jpg" />
+    </div>
+  );
+};
 
 export default MainPage;
